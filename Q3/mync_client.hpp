e2a8164ce6684d5fe9d1291c4a78ffe398a56e4b
Yair Cohen <Yair.cohen9@msmail.ariel.ac.il>
2024-06-21 17:16:44 +0300

    Add files via upload

---
+++ Q3/mync_client.hpp
@@ -0,0 +1,11 @@
+
+#ifndef MYNC_CLIENT_HPP
+#define MYNC_CLIENT_HPP
+
+#include <string>
+
+using std::string;
+
+int startTCPClient(const string &hostname, int port);
+
+#endif
---
+++ Q3/mync_server.hpp
@@ -0,0 +1,7 @@
+
+#ifndef MYNC_SERVER_HPP
+#define MYNC_SERVER_HPP
+
+int startTCPServer(int port);
+void redirectOutput(int socket);
+#endif
